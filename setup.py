85475a2b4fe6345b58c9726085256aef2a70f5e8
Austin <ajkite3@gmail.com>
2024-09-30 09:59:41 -0400

    fixed error with name in setup.py line 5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools_rust import RustExtension
 
 setup(
-    name="myproject",
+    name="v8",
     version="0.1",
     rust_extensions = [RustExtension("my_rust_lib.my_rust_lib", "Cargo.toml")],
     zip_safe = False,
