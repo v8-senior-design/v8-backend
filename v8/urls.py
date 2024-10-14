6be4ae7f216eef1ea02e12cc75d8bfbc146acb2b
FilipDimi <filipdimitrievski@protonmail.com>
2024-10-14 10:17:36 -0400

    Main link splits into 2, calc for calculating carbon footprint and user for auth

--- v8/urls.py
+++ v8/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('user/', include('user.urls')),
+    path('calc/', include('calc.urls')),
 ]
